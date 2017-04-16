1ba4f714709d866a12094007f5fc43f66409c3bf
albarqal <bruno_4556@hotmail.com>
2017-04-15 23:42:11 -0500

    Create DispensadorEfectivo.cs

---
+++ DispensadorEfectivo.cs
@@ -0,0 +1,29 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2014140087.Entities
+{
+    public class DispensadorEfectivo
+    {
+
+        private int contador = 500;
+
+        public void disminuir()
+        {
+            contador--;
+        }
+
+        public void incrementar()
+        {
+            contador++;
+        }
+
+        public int getContador()
+        {
+            return contador;
+        }
+    }
+}
